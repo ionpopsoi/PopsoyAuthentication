8d1c6c80eeef05b4277cee5185e510ebf0bf5c25
Ion popsoi <ionmanager@gmail.com>
2020-01-05 22:45:57 +0000

    Adicionado Login e Logout.

--- src/Controller/UsersController.js
+++ src/Controller/UsersController.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+var config = require('../Utils/Config');
 
 import * as UsersServices from '../Services/Users/UsersServices';
 import { VerifyToken } from '../Utils/JWTUtils'; 
@@ -13,9 +14,30 @@ router.post('/register', async(req,res,next)=> {
     res.send(ss);
 });
 
+//? POST
+// Login users
+//* TODO? Migrate this layer to UsersServices.js- 
+router.post('/login', async(req,res,next) => {
+    if(req.body.user === 'admin' && req.body.pwd === '123') {
+        const id = 1; //esse id viria do banco de dados
+        var token = jwt.sign({ id }, config.secret, {
+            expiresIn: 600 // 10min
+        });
+        res.status(200).send({ auth: true, token: token });
+    } else {
+        res.status(500).send('Login inválido!');
+    }
+});
+
+//? GET
+// Remover token do utilizador
+//* TODO? Delete token from database
+router.get('/logout', (req,res,next) => {
+    res.status(200).send({ auth: false, toke:null});
+})
+
 router.get('/test', VerifyToken, async(req,res,next)=> {
-    const ss = UsersServices.RegisterUsers();
-    res.send(ss);
+    res.send("User sucessfull authenticated.");
 });
 
 module.exports = router;
--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import express from 'express';
+require('dotenv').config()
+var bodyParser = require('body-parser')
+
 
 const UsersController = require('./Controller/UsersController');
 
 const app = express();
 const PORT = 8443;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api/v1/users', UsersController);
+
 
 app.listen(PORT, ()=> { console.log("SERVICE STARTED ON PORT", PORT)})
